22e39f295dd3493819930fc15c8f14034cb7745b
Nikhil <nikhil.4002.50.82@gmail.com>
2025-05-03 13:49:38 +0530

    small change

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -154,7 +154,7 @@ const Header = () => {
                            font-semibold rounded-3xl absolute -top-1 -right-1 
                            flex items-center justify-center text-xs"
               >
-                0
+                1
               </div>
             </div>
           </div>
